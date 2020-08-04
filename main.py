82257dcb3557b3f0fef83350ea8b6a2505059615
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-05 00:33:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ def sketch():
             g-=3
         elif i<255*5//3:
             r+=3
-        else:
+        elif i<255*6//3:
             b -=3
+        else:
+            continue
         fd(50+i)
         rt(91)
         pencolor(r,g,b)
 
 if __name__ == "__main__":
-    while True:
-        try:
-            sketch()
-        except KeyboardInterrupt:
-            print('this is working')
-            sketch()
-    time.sleep(5)
+    try:
+        sketch()
+    except KeyboardInterrupt:
+        print('this is working')
+        sketch()
